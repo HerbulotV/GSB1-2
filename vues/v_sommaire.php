edeeb65864580450f5569810e3c8cf90159d374e
HerbulotV <70969050+HerbulotV@users.noreply.github.com>
2021-05-17 18:13:41 +0200

    vues

---
+++ vues/v_sommaire.php
@@ -0,0 +1,30 @@
+<!-- Division pour le sommaire -->
+    <div id="menuGauche">
+     <div id="infosUtil">
+    
+        <h2>
+    
+</h2>
+    
+      </div>  
+        <ul id="menuList">
+			<li >
+				  Comptable :<br>
+				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+			</li>
+           <li class="smenu">
+              <a href="index.php?uc=validerFrais&action=choisirVisiteur" title="Valider fiche de frais ">Valider Frais</a>
+           </li>
+           <li class="smenu">
+             
+           </li>
+           <li class="smenu">
+              <a href="index.php?uc=paiement&action=choisirVisiteur" title="Passage Paiement">Passage Paiment</a>
+           </li>
+           
+ 	   <li class="smenu">
+              <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+           </li>
+         </ul>
+        
+    </div>
